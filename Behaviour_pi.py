fa18f9c5d2a027f03e2d0b319e02bcc6c690d67e
kevinkang <e0751516@u.nus.edu>
2025-04-12 14:53:17 -0400

    Add Policy Code

---
+++ Behaviour_pi.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+
+
+class Behavioural(nn.Module):
+    def __init__(self, input_dim=5, hidden_dim=32, output_dim=2):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, output_dim),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        return self.model(x)
+    
+class Policy(nn.Module):
+    def __init__(self, input_dim=5, hidden_dim=32, output_dim=2):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, output_dim),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        return self.model(x)
