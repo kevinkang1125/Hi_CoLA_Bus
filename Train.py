01715c3d2ec9f1ecd1eeb0e0c336ff1b5aef0162
kevinkang <e0751516@u.nus.edu>
2025-05-19 09:29:15 -0400

    Add KL divergence Tracking

--- Train.py
+++ Train.py
@@ -8,6 +8,12 @@ import matplotlib.pyplot as plt
 
 behavior_pi = Behavioural()
 behavior_pi.load_state_dict(torch.load("./Behavioural_model.pth"))
+
+base_pi = Behavioural()
+base_pi.load_state_dict(torch.load("./Behavioural_model.pth"))
+df = pd.read_csv("./traj.csv")
+states = torch.tensor(df.iloc[:60000, :5].values, dtype=torch.float32)
+
 input_dim = sum(p.numel() for p in behavior_pi.parameters())
 model = Hi_CoLA(input_dim=input_dim)
 model.load_state_dict(torch.load("./Hi_CoLA_net.pth"))
@@ -17,8 +23,20 @@ model.eval()
 def get_flat_params(model):
     return torch.cat([p.view(-1) for p in model.parameters()])
 
+def bernoulli_kl(q, p, eps=1e-8):
+    p = torch.clamp(p, eps, 1 - eps)
+    q = torch.clamp(q, eps, 1 - eps)
+    return (p * torch.log(p / q) + (1 - p) * torch.log((1 - p) / (1 - q))).sum(dim=-1)
+
+def kl_between_policies(pi_perturbed, pi, states):
+    with torch.no_grad():
+        probs_pi = pi(states)
+        probs_pert = pi_perturbed(states)
+        kl_vals = bernoulli_kl(probs_pert, probs_pi)
+    return kl_vals.mean()
+
 # Setup optimizer to update the behavioural model's parameters
-optimizer = torch.optim.Adam(behavior_pi.parameters(), lr=0.04)
+optimizer = torch.optim.Adam(behavior_pi.parameters(), lr=0.004)
 
 # Target KL divergence we want to achieve
 target_kl = torch.tensor([[1]], dtype=torch.float32)
@@ -26,6 +44,7 @@ target_kl = torch.tensor([[1]], dtype=torch.float32)
 # Optimization loop
 loss_fn = nn.MSELoss()
 loss_trace, kl_trace = [], []
+kl_divs = []
 
 for step in range(1200):
     optimizer.zero_grad()
@@ -40,19 +59,19 @@ for step in range(1200):
     loss = loss_fn(kl_output, target_kl)
     loss.backward()
     optimizer.step()
-    
+    kl_div = kl_between_policies(behavior_pi, base_pi, states)
     # Logging
     loss_trace.append(loss.item())
     kl_trace.append(kl_output.item())
+    kl_divs.append(kl_div.item())
     
     if step % 50 == 0 or step == 299:
         print(f"Step {step:03d} | Loss: {loss.item():.6f} | KL: {kl_output.item():.4f}")
 
-plt.figure(figsize=(14, 8))
+plt.figure(figsize=(16, 8))
 
 plt.subplot(1, 2, 1)
 plt.plot(loss_trace)
-# plt.title("Loss")
 plt.xlabel("Step",fontsize = 20)
 plt.ylabel("Loss",fontsize = 20)
 plt.xticks(fontsize = 15)
@@ -60,17 +79,15 @@ plt.yticks(fontsize = 15)
 plt.grid(True)
 
 
-
+kl_divs = np.minimum(kl_divs,3)
 plt.subplot(1, 2, 2)
-plt.plot(kl_trace)
-# plt.axhline(target_kl.item(), linestyle='--', color='r', label='Target KL')
-# plt.title("Confidence Lowerbound")
+plt.plot(kl_divs)
 plt.xlabel("Step",fontsize = 20)
-plt.ylabel("Predicted Confidence Lowerbound",fontsize = 20)
+plt.ylabel("Kl_Divergence_Track",fontsize = 20)
 plt.xticks(fontsize = 15)
 plt.yticks(fontsize = 15)
 plt.grid(True)
-
 # # plt.tight_layout()
 plt.show()
-torch.save(behavior_pi.state_dict(), "./Optimized_Behavioural_model.pth")
+# print(kl_divs)
+# torch.save(behavior_pi.state_dict(), "./Optimized_Behavioural_model.pth")
