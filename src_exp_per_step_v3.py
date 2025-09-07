c8ddec199a730561bf7d4a7ff7a398e1db98d16b
kevinkang <e0751516@u.nus.edu>
2025-09-07 14:53:58 -0400

    Update after improvement checking

---
+++ src_exp_per_step_v3.py
@@ -0,0 +1,150 @@
+import torch
+import torch.multiprocessing as mp
+import numpy as np
+from multiprocessing import Pool
+from tqdm import tqdm
+import gym
+import time
+import copy
+import flexible_bus
+import json
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
+import os
+from Behaviour_pi import Behavioural, Policy
+from src_Hi_CoLA import Hi_CoLA_Net
+import src_Hi_CoLA as hc
+
+import pandas as pd
+import torch.nn as nn
+from Simulation import traj_collect
+from src_utils import *
+from src_clb_cal import *
+
+
+def main():
+
+    traj_num = 300000#300000
+    gamma = 0.99
+    pertub_size = 400
+    target_count = 150
+    count = 0
+    sigma = 0.15
+    kl = 0.3
+    behavior_pi = Behavioural()
+    behavior_pi.load_state_dict(torch.load("./Behavioural_model.pth"))
+    env = gym.make('FlexibleBus-v0')
+
+    start_time = time.time()
+    result_rec = []
+    epoch_record = []
+    done = False
+    b_loop = 0
+    stop_signal = 2
+    while not done:
+        start_time = time.time()
+        traj_list, return_list = traj_collect(env=env, traj_num=traj_num, gamma=gamma, base_policy=behavior_pi)
+        states = torch.tensor(np.stack(traj_list["obs_0"].iloc[:60000].values), dtype=torch.float32)#60000
+
+        perturbs = []
+        thomas_lbs = []
+        # count = 0
+        bin_counts = {f"{round(start,1):.1f}-{round(start+0.1,1):.1f}": 0 for start in np.arange(0, kl, 0.1)}
+        # pi_perturbed = perturb_add_v2(pi=behavior_pi, c=sigma)
+        # kl_score = kl_between_policies(pi_perturbed, behavior_pi, states).item()
+        # print(f"KL score: {kl_score:.4f}")
+
+        while not all(count >= target_count for count in bin_counts.values()):
+            pi_perturbed = perturb_add_v2(pi=behavior_pi, c=sigma)
+            kl_score = kl_between_policies(pi_perturbed, behavior_pi, states).item()
+            if kl_score <= kl:
+                kl_range = get_kl_bin(kl_score)
+                if bin_counts[kl_range] < target_count:
+                    bin_counts[kl_range] += 1
+                    count += 1
+                    perturbs.append(copy.deepcopy(pi_perturbed))
+                    save_path = f"./Policys/Whole_Loop/"
+                    os.makedirs(save_path, exist_ok=True)
+                    filename = f"Perturbed_model_{count}.pth"
+                    torch.save(pi_perturbed.state_dict(), os.path.join(save_path, filename))
+                    print(bin_counts)
+
+        for pi in tqdm(perturbs, desc="Calculating Thomas LBs"):
+            lb = Thomas_hc(behavior_pi, pi, traj_list, return_list, epochs=int(0.8 * traj_num), confidence_level=0.9)
+            thomas_lbs.append(lb)
+
+        input_dim = sum(p.numel() for p in behavior_pi.parameters())
+        hi_cola, _, _ = hc.train(
+            features=perturbs,
+            labels=thomas_lbs,
+            model=Hi_CoLA_Net(input_dim=input_dim),
+            lr=1e-6,
+            weight_decay=1e-4,
+            epochs=1000,
+            verbose=False,
+            logit=False,
+            train_ratio=0.7
+        )
+
+        optimizer = torch.optim.Adam(behavior_pi.parameters(), lr=0.004)
+        target_lb = torch.tensor([[1]], dtype=torch.float32)
+
+        loss_fn = nn.MSELoss()
+        loss_trace, return_trace = [], []
+        start_pi = copy.deepcopy(behavior_pi)
+        output_pi = None
+        for step in range(300):
+            optimizer.zero_grad()
+            device = next(hi_cola.parameters()).device
+            input_vector = get_flat_params(behavior_pi).unsqueeze(0).to(device)
+            confidence_lb = hi_cola(input_vector)
+            loss = loss_fn(confidence_lb, target_lb)
+            loss.backward()
+            optimizer.step()
+            return_trace.append(confidence_lb.item())
+
+            device = next(behavior_pi.parameters()).device
+            start_pi = start_pi.to(device)
+            test_states = states.to(device)
+            traj_list, return_list = traj_collect(env=env, traj_num=traj_num, gamma=gamma, base_policy=copy.deepcopy(behavior_pi).cpu())
+            np.savetxt(f"./exp_per_step/perturb_v2/raw_reward_{b_loop}_{step}.txt", return_list, delimiter=",")
+            if kl_between_policies(behavior_pi, start_pi, test_states) > 0.25:
+                break
+            output_pi = copy.deepcopy(behavior_pi)
+            # traj_list, return_list = traj_collect(env=env, traj_num=traj_num, gamma=gamma, base_policy=copy.deepcopy(output_pi))
+            # np.savetxt(f"./exp_per_step/raw_reward_{b_loop}_{step}.txt", return_list, delimiter=",")
+
+        epoch_record.append(confidence_lb.item())
+        behavior_pi = copy.deepcopy(output_pi)
+        result_rec.extend(return_trace)
+        if np.var(result_rec[-200:]) < 0.001 * np.mean(result_rec[-200:]):
+            print("Converged")
+            done = True
+
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"train time per epoch: {elapsed_time:.4f} seconds")
+        b_loop += 1
+        if b_loop >= stop_signal:
+            done = True
+        print(f"Behavioral Loop {b_loop} finished, confidence lowerbound: {confidence_lb.item():.4f}")
+        behavior_pi = copy.deepcopy(behavior_pi).cpu()
+        np.savetxt("./exp_per_step/perturb_v2/Confidence_Lowerbound_Record.txt", result_rec, delimiter=",")
+        np.savetxt("./exp_per_step/perturb_v2/Epoch_Record.txt", epoch_record, delimiter=",")
+
+    # Plotting
+    import matplotlib.pyplot as plt
+    plt.plot(result_rec, label='Confidence Lowerbound')
+    plt.xlabel("Step", fontsize=20)
+    plt.xticks(fontsize=15)
+    plt.yticks(fontsize=15)
+    plt.grid(True)
+    plt.show()
+
+    np.savetxt("./exp_per_step/perturb_v2/Confidence_Lowerbound_Record.txt", result_rec, delimiter=",")
+    np.savetxt("./exp_per_step/perturb_v2/Epoch_Record.txt", epoch_record, delimiter=",")
+    torch.save(behavior_pi.state_dict(), "./exp_per_step/perturb_v2/Optimized_Behavioural_model.pth")
+
+if __name__ == "__main__":
+    torch.multiprocessing.set_start_method('spawn')
+    main()
--- src_utils.py
+++ src_utils.py
@@ -34,7 +34,7 @@ def perturb_add_v2(pi, c=0.3):
     with torch.no_grad():
         for param in pi_perturbed.parameters():
             if param.requires_grad:
-                sigma = c * param.abs()
+                sigma = torch.sqrt(c * param.abs() + 1e-8)
                 noise = torch.randn_like(param) * sigma
                 param.add_(noise)
 
@@ -54,31 +54,25 @@ def get_flat_params(model):
     # Flatten the Behavioural model's parameters into a vector
     return torch.cat([p.view(-1) for p in model.parameters()])
 
-# def kl_filter(pi, traj, sigma=0.3, kl_threshold=0.3):
-#     df = pd.read_csv(traj)
-#     states = torch.tensor(df.iloc[:60000, :5].values, dtype=torch.float32)
-#     behavior_pi = Behavioural()
-#     behavior_pi.load_state_dict(torch.load("./Behavioural_model_2.pth"))
+def get_kl_bin(kl_score):
+    bins = np.arange(0, 0.3, 0.1)
+    for start in bins:
+        end = round(start + 0.1, 1)
+        if start <= kl_score < end:
+            return f"{start:.1f}-{end:.1f}"
+    return None
 
-#     target_count = 200
-#     bin_counts = {f"{round(start,1):.1f}-{round(start+0.1,1):.1f}": 0 for start in np.arange(0, 0.5, 0.1)}
-#     bin_indices = {k: 0 for k in bin_counts.keys()}  # for naming files
 
-#     sigma = 0.4
+# def kl_filter(kl_score,kl_threshold=0.3):
+#     # target_count = 200
+#     # bin_counts = {f"{round(start,1):.1f}-{round(start+0.1,1):.1f}": 0 for start in np.arange(0, 0.3, 0.1)}
 
-    # while not all(count >= target_count for count in bin_counts.values()):
-    #     pi_perturbed = perturb_add(pi=behavior_pi, sigma=sigma)
-    #     kl_score = kl_between_policies(pi_perturbed, behavior_pi, states).item()
 
-    #     if kl_score < 0.3:
-    #         folder = get_kl_folder(kl_score)
-    #         if folder and bin_counts[folder] < target_count:
-    #             save_path = f"./Policys/{folder}/"
-    #             os.makedirs(save_path, exist_ok=True)
-    #             filename = f"Perturbed_model_{bin_indices[folder]}.pth"
-    #             torch.save(pi_perturbed, os.path.join(save_path, filename))
-    #             bin_counts[folder] += 1
-    #             bin_indices[folder] += 1
-    #             print(f"Saved to {folder}/{filename} (KL={kl_score:.4f}) — count: {bin_counts[folder]}/{target_count}")
-    # pass
+#     # while not all(count >= target_count for count in bin_counts.values()):
+
+#     #     if kl_score < kl_threshold:
+#     #         folder = get_kl_bin(kl_score)
+#     #         if folder and bin_counts[folder] < target_count:
+#     #             bin_counts[folder] += 1
+
 
